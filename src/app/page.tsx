e5d82fb6b40939dc597251fc06a5b4d5311e36de
Christian Liebhardt <christian.liebhardt@arcor.de>
2024-11-07 17:47:48 +0100

    Fixed linter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,7 +100,7 @@ function InstallPrompt() {
 
 	useEffect(() => {
 		setIsIOS(
-			/iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
+			/iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream // eslint-disable-line @typescript-eslint/no-explicit-any
 		);
 
 		setIsStandalone(window.matchMedia('(display-mode: standalone)').matches);
@@ -121,7 +121,7 @@ function InstallPrompt() {
 						{' '}
 						⎋{' '}
 					</span>
-					and then "Add to Home Screen"
+					and then &quot;Add to Home Screen&quot;
 					<span role="img" aria-label="plus icon">
 						{' '}
 						➕{' '}
