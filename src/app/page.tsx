08685446dcf63b448ad05a449b4e27309caed347
Christian Liebhardt <christian.liebhardt@arcor.de>
2024-11-07 18:13:53 +0100

    Less noise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,8 @@ function InstallPrompt() {
 
 		setIsStandalone(window.matchMedia('(display-mode: standalone)').matches);
 
-		const handleBeforeInstallPrompt = (e) => {
+		const handleBeforeInstallPrompt = (e: any) => {
+			// eslint-disable-line @typescript-eslint/no-explicit-any
 			e.preventDefault(); // Prevent automatic prompt
 			setDeferredPrompt(e); // Save the event to trigger later
 		};
@@ -165,7 +166,7 @@ function InstallPrompt() {
 			)}
 			{!isIOS && deferredPrompt && (
 				<p>
-					Click "Add to Home Screen" to install this app on your Android device.
+					Click Add to Home Screen to install this app on your Android device.
 				</p>
 			)}
 		</div>
@@ -186,64 +187,7 @@ export default function Home() {
 					height={38}
 					priority
 				/>
-				<ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-					<li className="mb-2">
-						Get started by editing{' '}
-						<code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-							src/app/page.tsx
-						</code>
-						.
-					</li>
-					<li>Save and see your changes instantly.</li>
-				</ol>
 			</main>
-			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/file.svg"
-						alt="File icon"
-						width={16}
-						height={16}
-					/>
-					Learn
-				</a>
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/window.svg"
-						alt="Window icon"
-						width={16}
-						height={16}
-					/>
-					Examples
-				</a>
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/globe.svg"
-						alt="Globe icon"
-						width={16}
-						height={16}
-					/>
-					Go to nextjs.org →
-				</a>
-			</footer>
 		</div>
 	);
 }
